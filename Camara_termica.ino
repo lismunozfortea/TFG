0b4fa2986a8b7d407c6c296faa2e8c80aa53fc21
lismunozfortea <47848039+lismunozfortea@users.noreply.github.com>
2022-07-07 20:30:16 +0200

    Implementadas las interrupciones
    Se han implementado los calculos de los PID's y las lecturas/escrituras de los sensores como rutinas de servicio, utilizando interrupciones de software. Ahora mismo se guardan los resultados en variables, falta hacer que en vez de eso se guarden en los vectores globales de entradas y salidas. Falta implementar la función de control del puente H.

--- Camara_termica.ino
+++ Camara_termica.ino
@@ -8,6 +8,7 @@ float salidas_corriente[2]; //vector de salidas de corriente
 float entradas_corriente[2]; //vector de entradas de corriente
 // Variables auxiliares
 float temp_seleccionada; //temperatura de consigna que es la que ha elegido el usuario
+float corriente_deseada; //corriente de consigna para el regulador PID de corriente
 float salida_temp_reg; //salida de temperatura después de la acción del regulador de temperatura
 float salida_corriente_reg; //salida de corriente después de la acción del regulador de corriente
 float lectura_tempC; //entre 0 y 4095
@@ -21,20 +22,28 @@ bool leidos=false;
 const int pin_tempF = 32;
 const int pin_tempC = 33;
 const int pin_corriente = 34;
-
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-//  pinMode(PIN, INPUT_PULLUP); //revisa esto
- // attachInterrupt(PIN, PID(salidas_temp,entradas_temp,temp_seleccionada), CHANGE); //establece la interrupción cuando pasa algo en el pin que sea (pon el pin al que vaya el sensor)
+//Variables para las interrupciones
+volatile int contador1;
+volatile int contador2;
+hw_timer_t * timer1 = NULL;
+portMUX_TYPE timerMux1 = portMUX_INITIALIZER_UNLOCKED;
+hw_timer_t * timer2 = NULL;
+portMUX_TYPE timerMux2 = portMUX_INITIALIZER_UNLOCKED;
+//Funciones ISR
+void IRAM_ATTR onTimer1() { //ISR para el manejo de la interrupción de los PID's
+  portENTER_CRITICAL_ISR(&timerMux1);
+  contador1++;
+  portEXIT_CRITICAL_ISR(&timerMux1);
+ 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-//detachInterrupt(GPIOPin); //desconecta la interrupción (añadir la condición)
+void IRAM_ATTR onTimer2() { //ISR para el manejo de la interrupción de los sensores
+  portENTER_CRITICAL_ISR(&timerMux2);
+  contador2++;
+  portEXIT_CRITICAL_ISR(&timerMux2);
+ 
 }
-
-float PIDt(float u[2], float e[2], float consigna){
+//Funcion para calcular los reguladores PID
+float PID(float u[2], float e[2], float consigna){
 //Ec en diferencias del PID: u(k)=u(k−1)+q0e(k)+q1e(k-1)+q2e(k-2)   (siendo "u" la salida del lazo y "e" la entrada)
 float q0,q1,q2;
 q0=q1=q2=1;
@@ -49,6 +58,7 @@ q0=q1=q2=1;
    
 }
 
+//Funciones para la lectura/escritura de los valores de los sensores
 void LecturaSensores(){ //lee de los pines ADC el valor de los sensores, estos pines tienen resolución de 12 bits, leen de 0 a 4095 donde 0 es 0V y 4095 3.3V
 lectura_tempF= analogRead(pin_tempF)* (3.3 / 4095.0);
 delay(1000);
@@ -69,5 +79,51 @@ valor_tempF= lectura_tempF/sensibilidadT;
 valor_tempC= lectura_tempC/sensibilidadT;
 //Valor sensor de corriente
 valor_corriente= (lectura_corriente-2.5)/sensibilidadC; //formula desarrollada en la memoria
+}
+
+}
+void setup() {
+Serial.begin(115200);
+//Inicializacion de los temporizadores
+timer1 = timerBegin(0, 80, true); //la frecuencia base utilizada por los contadores en el ESP32 es de 80MHz
+timer2 = timerBegin(0, 80, true);
+//Manejo de los temporizadores
+timerAttachInterrupt(timer1, &onTimer1, true);
+timerAttachInterrupt(timer2, &onTimer2, true);
+//Valor de los contadores
+timerAlarmWrite(timer1, 1000000, true); //el segundo parámetro nos indica cada cuanto se generará la interrupción, en este caso cada un segundo
+timerAlarmWrite(timer2, 1000000, true);
+//Habilitación de los contadores
+timerAlarmEnable(timer1);
+timerAlarmEnable(timer2);
+
+}
+
+void loop() {
+
+  //Interrupcion de los PID's
+if (contador1>0) {
+ portENTER_CRITICAL(&timerMux1);
+    contador1--;
+    
+    portEXIT_CRITICAL(&timerMux1);
+
+    //Código para el manejo de la interrupción
+
+    salida_temp_reg= PID(salidas_temp,entradas_temp,temp_seleccionada);
+    salida_corriente_reg= PID(salidas_corriente,entradas_corriente,corriente_deseada); //falta ver como se calcula la corriente deseada
+    
+}
+ //Interrupcion de los sensores
+if (contador2>0) {
+ portENTER_CRITICAL(&timerMux2);
+    contador2--;
+    
+    portEXIT_CRITICAL(&timerMux2);
+
+    //Código para el manejo de la interrupción
+LecturaSensores();
+ValorSensores();
+    
 }
 }
