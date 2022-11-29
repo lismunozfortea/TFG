6e3ee0e4bd0cfc48360582242d7bd82f5c3ffe6d
lismunozfortea <47848039+lismunozfortea@users.noreply.github.com>
2022-11-29 21:03:36 +0100

    Versión 4.0. Incluye la nueva interfaz con LVGL.
    Código completo que incluye la nueva interfaz implementada con LVGL.

--- Software_TFG.ino
+++ Software_TFG.ino
@@ -1,83 +1,36 @@
 #include <analogWrite.h>
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+
+//*********VARIABLES DE LA INTERFAZ********//
+
+/*Change to your screen resolution*/
+static const uint16_t screenWidth  = 320;
+static const uint16_t screenHeight = 240;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[screenWidth * 10];
+
+TFT_eSPI tft(screenWidth, screenHeight); /* TFT instance */
+
+lv_obj_t *spinbox;
+lv_obj_t *led1;
+lv_obj_t *chart;
 
 
-/*
-  The TFT_eSPI library incorporates an Adafruit_GFX compatible
-  button handling class, this sketch is based on the Arduin-o-phone
-  example.
-  This example diplays a keypad where numbers can be entered and
-  send to the Serial Monitor window.
-  The sketch has been tested on the ESP8266 (which supports SPIFFS)
-  The minimum screen size is 320 x 240 as that is the keypad size.
-  TOUCH_CS and SPI_TOUCH_FREQUENCY must be defined in the User_Setup.h file
-  for the touch functions to do anything.
-*/
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void dbg_print(const char * buf)
+{
+    Serial.printf(buf);
+    Serial.flush();
+}
+#endif
 
-// The SPIFFS (FLASH filing system) is used to hold touch screen
-// calibration data
 
-#include "FS.h"
+           
+//------------------------------------------------------------
 
-#include <SPI.h>
-#include <TFT_eSPI.h>      // Hardware-specific library
-
-TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
-
-// This is the file name used to store the calibration data
-// You can change this to create new calibration files.
-// The SPIFFS file name must start with "/".
-#define CALIBRATION_FILE "/TouchCalData2"
-
-// Set REPEAT_CAL to true instead of false to run calibration
-// again, otherwise it will only be done once.
-// Repeat calibration if you change the screen rotation.
-#define REPEAT_CAL false
-
-// Keypad start position, key sizes and spacing
-#define KEY_X 40 // Centre of key
-#define KEY_Y 96
-#define KEY_W 62 // Width and height
-#define KEY_H 30
-#define KEY_SPACING_X 18 // X and Y gap
-#define KEY_SPACING_Y 20
-#define KEY_TEXTSIZE 1   // Font size multiplier
-
-// Using two fonts since numbers are nice when bold
-#define LABEL1_FONT &FreeSansOblique12pt7b // Key label font 1
-#define LABEL2_FONT &FreeSansBold12pt7b    // Key label font 2
-
-// Numeric display box size and location
-#define DISP_X 1
-#define DISP_Y 10
-#define DISP_W 238
-#define DISP_H 50
-#define DISP_TSIZE 3
-#define DISP_TCOLOR TFT_CYAN
-
-// Number length, buffer for storing it and character index
-#define NUM_LEN 12
-char numberBuffer[NUM_LEN + 1] = "";
-uint8_t numberIndex = 0;
-
-// We have a status line for messages
-#define STATUS_X 120 // Centred on this
-#define STATUS_Y 65
-
-// Cuadro donde aparece la temperatura lado frio
-#define TEMP_X 250 // Centred on this
-#define TEMP_Y 65
-
-// Create 15 keys for the keypad
-char keyLabel[15][5] = {"New", "Del", "Send", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0", "#" };
-uint16_t keyColor[15] = {TFT_RED, TFT_DARKGREY, TFT_DARKGREEN,
-                         TFT_BLUE, TFT_BLUE, TFT_BLUE,
-                         TFT_BLUE, TFT_BLUE, TFT_BLUE,
-                         TFT_BLUE, TFT_BLUE, TFT_BLUE,
-                         TFT_BLUE, TFT_BLUE, TFT_BLUE
-                        };
-
-// Invoke the TFT_eSPI button class and create all the button objects
-TFT_eSPI_Button key[15];
 //*********VARIABLES GLOBALES********//
 // Vectores de entradas y salidas //
 //en la posicion 2 el valor actual, en la posicion 1 el valor un instante de muestreo atrás y en la 0 el valor dos instantes de muestreo atrás
@@ -110,7 +63,73 @@ volatile int contador;
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-//Funciones ISR //
+
+//*********FUNCIONES INTERFAZ********//
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+    tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, w, h);
+    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
+    tft.endWrite();
+
+    lv_disp_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+    uint16_t touchX, touchY;
+
+    //bool touched = tft.getTouch(&touchX, &touchY, 600);
+    bool touched = tft.getTouch(&touchX, &touchY);
+    if(!touched)
+        data->state = LV_INDEV_STATE_REL;
+    else
+    {
+        data->state = LV_INDEV_STATE_PR;
+
+        /*Set the coordinates*/
+        data->point.x = touchX+45;
+        data->point.y = touchY;
+
+        Serial.print("Data x ");
+        Serial.println(touchX);
+
+        Serial.print("Data y ");
+        Serial.println(touchY);
+    }
+}
+
+//-------------/*Callbacks de eventos*/------------------------------------
+static void lv_spinbox_increment_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code == LV_EVENT_SHORT_CLICKED || code  == LV_EVENT_LONG_PRESSED_REPEAT) {
+        lv_spinbox_increment(spinbox);
+    }
+}
+
+static void lv_spinbox_decrement_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code == LV_EVENT_SHORT_CLICKED || code == LV_EVENT_LONG_PRESSED_REPEAT) {
+        lv_spinbox_decrement(spinbox);
+    }
+}
+//Cada vez que se modifique el valor del objeto ocurrirá el evento
+static void lv_spinbox_value_event_cb(lv_event_t * e)
+{
+     
+ temp_seleccionada = lv_spinbox_get_value(spinbox);
+ 
+ }
+
+////////////*Funciones ISR* //////////////////////////////
 void IRAM_ATTR onTimer() { //ISR para el manejo de la interrupción de los PID's
   portENTER_CRITICAL_ISR(&timerMux);
   contador++;
@@ -123,31 +142,122 @@ void setup() {
   // Use serial port
   Serial.begin(115200);
 
-  // Initialise the TFT screen
-  tft.init();
+  
+   //*********SETUP INTERFAZ LVGL********//
 
-  // Set the rotation before we calibrate
-  tft.setRotation(3);
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
-  // Calibrate the touch screen and retrieve the scaling factors
-  //touch_calibrate();
-  uint16_t calData[5] = {299, 3588, 348, 3474, 1};
-  tft.setTouch(calData);
+    Serial.println(LVGL_Arduino);
+    Serial.println("I am LVGL_Arduino");
 
-  // Clear the screen
-  tft.fillScreen(TFT_BLACK);
+    lv_init();
 
-  // Draw keypad background
-  tft.fillRect(0, 0, 240, 320, TFT_DARKGREY);
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb(dbg_print); /* register print function for debugging */
+#endif
 
-  // Draw number display area and frame
-  tft.fillRect(DISP_X, DISP_Y, DISP_W, DISP_H, TFT_BLACK);
-  tft.drawRect(DISP_X, DISP_Y, DISP_W, DISP_H, TFT_WHITE);
+    tft.begin();         /* TFT init */
+    tft.setRotation(3);  /* Landscape orientation, flipped */
 
-  // Draw keypad
-  drawKeypad();
-  //Status inicial
-  status("Introduzca temperatura"); 
+    /*Set the touchscreen calibration data,
+     the actual data for your display can be acquired using
+     the Generic -> Touch_calibrate example from the TFT_eSPI library*/
+    uint16_t calData[5] = { 444, 3356, 366, 3336, 1};
+    tft.setTouch(calData);
+
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * 10);
+
+    /*Initialize the display*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    /*Change the following line to your display resolution*/
+    disp_drv.hor_res  = screenWidth;
+    disp_drv.ver_res  = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /*Initialize the (dummy) input device driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    /* Create simple label */
+    spinbox = lv_spinbox_create(lv_scr_act());
+    lv_spinbox_set_range(spinbox, -400, 700);
+    lv_spinbox_set_digit_format(spinbox, 3, 2);
+    lv_spinbox_step_prev(spinbox);
+    lv_obj_set_width(spinbox, 55);
+    lv_obj_align(spinbox, LV_ALIGN_TOP_MID, 0, 5);
+    lv_obj_add_event_cb(spinbox,lv_spinbox_value_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
+    lv_coord_t h = lv_obj_get_height(spinbox);
+
+    lv_obj_t *btn = lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn, 3*h/4, h);
+    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
+    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_PLUS, 0);
+    lv_obj_add_event_cb(btn, lv_spinbox_increment_event_cb, LV_EVENT_ALL,  NULL);
+
+    btn = lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn, 3*h/4, h);
+    lv_obj_align_to(btn, spinbox, LV_ALIGN_OUT_LEFT_MID, -5, 0);
+    lv_obj_set_style_bg_img_src(btn, LV_SYMBOL_MINUS, 0);
+    lv_obj_add_event_cb(btn, lv_spinbox_decrement_event_cb, LV_EVENT_ALL, NULL);
+
+    /*Create a LED and switch it OFF*/
+    led1 = lv_led_create(lv_scr_act());
+    lv_obj_align_to(led1, btn, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
+    lv_led_set_brightness(led1, 150);
+    lv_led_set_color(led1, lv_palette_main(LV_PALETTE_RED));
+    lv_led_off(led1);
+
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "Overheat");
+    lv_obj_align_to(label, led1, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
+
+    /*Create a chart*/
+    chart = lv_chart_create(lv_scr_act());
+    lv_obj_set_size(chart, 90, 140);
+    lv_obj_align_to(chart, spinbox, LV_ALIGN_OUT_BOTTOM_MID, 15, 45);
+    lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
+    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, -40, 80); 
+    lv_chart_set_axis_tick(chart, LV_CHART_AXIS_PRIMARY_Y, 10, 5, 7, 2, true, 35);
+
+    /*Add two data series*/
+    lv_chart_series_t *ser1 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+    lv_chart_series_t *ser2 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_GREEN), LV_CHART_AXIS_SECONDARY_Y);
+
+/////////////////////////////////////////////////////////////////////////ACTUALIZAR CON TEMPERATURA EN LOOP//////////////////////
+    /*Set the next points on 'ser1'*/
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 30);
+    lv_chart_set_next_value(chart, ser1, 70);
+    lv_chart_set_next_value(chart, ser1, 80);
+
+    /*Directly set points on 'ser2'*/
+    ser2->y_points[0] = 80;
+    ser2->y_points[1] = 70;
+    ser2->y_points[2] = 65;
+    ser2->y_points[3] = 65;
+    ser2->y_points[4] = 65;
+    ser2->y_points[5] = 65;
+    ser2->y_points[6] = 65;
+    ser2->y_points[7] = 65;
+    ser2->y_points[8] = 65;
+    ser2->y_points[9] = 65;
+
+    lv_chart_refresh(chart); /*Required after direct set*/
+ //////////////////////////////////////////////////////////////////////////////ACTUALIZAR CON TEMPERATURA EN LOOP/////////////////7
    //*********SETUP GENERAL********//
 
   //Setup pines puente H 
@@ -172,87 +282,10 @@ timerAlarmEnable(timer);
 
 void loop(void) {
  
+  lv_timer_handler(); /* let the GUI do its work */
 
-  uint16_t t_x = 0, t_y = 0; // To store the touch coordinates
+    delay(5); //Igual hay que quitarlo!!!
 
-  // Pressed will be set true is there is a valid touch on the screen
-  boolean pressed = tft.getTouch(&t_x, &t_y);
-
- 
-  // / Check if any key coordinate boxes contain the touch coordinates
-  for (uint8_t b = 0; b < 15; b++) {
-    if (pressed && key[b].contains(t_x, t_y)) {
-      key[b].press(true);  // tell the button it is pressed
-    } else {
-      key[b].press(false);  // tell the button it is NOT pressed
-    }
-  }
-
-  // Check if any key has changed state
-  for (uint8_t b = 0; b < 15; b++) {
-
-    if (b < 3) tft.setFreeFont(LABEL1_FONT);
-    else tft.setFreeFont(LABEL2_FONT);
-
-    if (key[b].justReleased()) key[b].drawButton();     // draw normal
-
-    if (key[b].justPressed()) {
-      key[b].drawButton(true);  // draw invert
-
-      // if a numberpad button, append the relevant # to the numberBuffer
-      if (b >= 3) {
-        if (numberIndex < NUM_LEN) {
-          numberBuffer[numberIndex] = keyLabel[b][0];
-          numberIndex++;
-          numberBuffer[numberIndex] = 0; // zero terminate
-        }
-       status("Introduzca temperatura"); // Clear the old status
-      }
-
-      // Del button, so delete last char
-      if (b == 1) {
-        numberBuffer[numberIndex] = 0;
-        if (numberIndex > 0) {
-          numberIndex--;
-          numberBuffer[numberIndex] = 0;//' ';
-        }
-        status("Introduzca temperatura"); // Clear the old status
-      }
-
-      if (b == 2) {
-        status("Temperatura enviada, espere");
-        Serial.println(numberBuffer);
-       temp_seleccionada= atof(numberBuffer);
-
-         //funcion para que aparezca un aviso cuando la temperatura se estabilice
-         if(entradas_temp[2]==temp_seleccionada) //mas bien seria cuando dejara de variar pero provisionalmente pongo esto
-         {
-          status("Temperatura estable, apunte los resultados");
-          delay(10000);
-                 }    
-      }
-      // we dont really check that the text field makes sense
-      // just try to call
-      if (b == 0) {
-        status("Introduzca temperatura");
-        numberIndex = 0; // Reset index to 0
-        numberBuffer[numberIndex] = 0; // Place null in buffer
-      }
-
-      // Update the number display field
-      tft.setTextDatum(TL_DATUM);        // Use top left corner as text coord datum
-      tft.setFreeFont(&FreeSans18pt7b);  // Choose a nicefont that fits box
-      tft.setTextColor(DISP_TCOLOR);     // Set the font colour
-
-      // Draw the string, the value returned is the width in pixels
-      int xwidth = tft.drawString(numberBuffer, DISP_X + 4, DISP_Y + 12);
-
-      // Now cover up the rest of the line up by drawing a black rectangle.  No flicker this way
-      // but it will not work with italic or oblique fonts due to character overlap.
-      tft.fillRect(DISP_X + 4 + xwidth, DISP_Y + 1, DISP_W - xwidth - 5, DISP_H - 2, TFT_BLACK);
-
-      delay(10); // UI debouncing
-    }
       //*********MANEJO DE INTERRUPCIONES********//
 if (contador>0) {
  portENTER_CRITICAL(&timerMux);
@@ -260,6 +293,7 @@ if (contador>0) {
     portEXIT_CRITICAL(&timerMux);
 
     //Código que se ejecuta durante la interrupción
+
 salidas_temp[0]=salidas_temp[1];
 salidas_temp[1]=salidas_temp[2];
 salidas_temp[2]= PID(salidas_temp,entradas_temp,temp_seleccionada,q_temp); //obtenemos la salidad el PID de temperatura en valor digital
@@ -271,125 +305,10 @@ LecturaSensores();
 ValorSensores();
 ciclo_trabajo=entradas_corriente[2]-corriente_deseada; //le pasamos el error de la corriente como ciclo de trabajo 
 ControlPuenteH(ciclo_trabajo);
-//Visualizacion temperatura
- PrintTemp();
-
 }
-  }
+  
  }
 
-//*********FUNCIONES PARA LA INTERFAZ********//
-//------------------------------------------------------------------------------------------
-
-void drawKeypad()
-{
-  // Draw the keys
-  for (uint8_t row = 0; row < 5; row++) {
-    for (uint8_t col = 0; col < 3; col++) {
-      uint8_t b = col + row * 3;
-
-      if (b < 3) tft.setFreeFont(LABEL1_FONT);
-      else tft.setFreeFont(LABEL2_FONT);
-
-      key[b].initButton(&tft, KEY_X + col * (KEY_W + KEY_SPACING_X),
-                        KEY_Y + row * (KEY_H + KEY_SPACING_Y), // x, y, w, h, outline, fill, text
-                        KEY_W, KEY_H, TFT_WHITE, keyColor[b], TFT_WHITE,
-                        keyLabel[b], KEY_TEXTSIZE);
-      key[b].drawButton();
-    }
-  }
-}
-
-//------------------------------------------------------------------------------------------
-
-void touch_calibrate()
-{
-  uint16_t calData[5];
-  uint8_t calDataOK = 0;
-
-  // check file system exists
-  if (!SPIFFS.begin()) {
-    Serial.println("Formating file system");
-    SPIFFS.format();
-    SPIFFS.begin();
-  }
-
-  // check if calibration file exists and size is correct
-  if (SPIFFS.exists(CALIBRATION_FILE)) {
-    if (REPEAT_CAL)
-    {
-      // Delete if we want to re-calibrate
-      SPIFFS.remove(CALIBRATION_FILE);
-    }
-    else
-    {
-      File f = SPIFFS.open(CALIBRATION_FILE, "r");
-      if (f) {
-        if (f.readBytes((char *)calData, 14) == 14)
-          calDataOK = 1;
-        f.close();
-      }
-    }
-  }
-
-  if (calDataOK && !REPEAT_CAL) {
-    // calibration data valid
-    tft.setTouch(calData);
-  } else {
-    // data not valid so recalibrate
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(20, 0);
-    tft.setTextFont(2);
-    tft.setTextSize(1);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
-    tft.println("Touch corners as indicated");
-
-    tft.setTextFont(1);
-    tft.println();
-
-    if (REPEAT_CAL) {
-      tft.setTextColor(TFT_RED, TFT_BLACK);
-      tft.println("Set REPEAT_CAL to false to stop this running again!");
-    }
-
-    tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
-
-    tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.println("Calibration complete!");
-
-    // store data
-    File f = SPIFFS.open(CALIBRATION_FILE, "w");
-    if (f) {
-      f.write((const unsigned char *)calData, 14);
-      f.close();
-    }
-  }
-}
-
-//------------------------------------------------------------------------------------------
-
-// Print something in the mini status bar
-void status(const char *msg) {
-  tft.setTextPadding(240);
-  //tft.setCursor(STATUS_X, STATUS_Y);
-  tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
-  tft.setTextFont(0);
-  tft.setTextDatum(TC_DATUM);
-  tft.setTextSize(1);
-  tft.drawString(msg, STATUS_X, STATUS_Y);
-}
-// Print temp
-void PrintTemp() {
-  tft.setCursor(TEMP_X, TEMP_Y);//Posición del texto en la pantalla
-  tft.setTextColor(ILI9341_GREEN);//Setea el color del texto en verde
-  tft.setTextSize(1);//Seteo del tamaño del texto
-  tft.println("Temp:");// Se imprime en patalla la "Temp:"
-   tft.setCursor(TEMP_X, (TEMP_Y+30));//Posición del texto en la pantalla
-   tft.fillRect(TEMP_X,TEMP_Y+12, 100, 50, ILI9341_BLACK);
-  tft.setTextColor(ILI9341_WHITE);//Setea el color del texto en blanco
-  tft.print(entradas_temp[2]);//Muestra la temperatura obtenida del sensor
-}
 
 //------------------------------------------------------------------------------------------
 //*********FUNCIONES GENERALES********//
