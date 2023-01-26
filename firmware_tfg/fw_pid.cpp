63834cf489973bd4382da8e640cae700b19996a3
Pedro Luis Castedo Cepeda <pedroluis.castedo@upm.es>
2023-01-26 02:26:54 +0100

    feature: add anti-wind-up to pid controller

--- firmware_tfg/fw_pid.cpp
+++ firmware_tfg/fw_pid.cpp
@@ -10,6 +10,10 @@ PidController::PidController(double ref, double sat, const struct PidParameters&
       -params.Kp + 0.5 * params.Ki * params.T - 2.0 * params.Kd / params.T,
        params.Kd / params.T
     },
+    m_csat {
+       params.Kp + params.Kd / params.T,
+       params.Kd / params.T
+    },
     m_e {0.0, 0.0, 0.0},
     m_y {0.0f},
     m_ref {ref},
--- firmware_tfg/fw_pid.hpp
+++ firmware_tfg/fw_pid.hpp
@@ -12,15 +12,18 @@ namespace FW {
     class PidController
     {
         double m_coef[3];
+        double m_csat[2];
         double m_e[3];
         double m_y[1];
         double m_ref;
         double m_sat;
+        bool   m_saturated = false;
     public:
         PidController(double ref, double sat, const struct PidParameters& params);
         double output(double input);
         double reference() const { return m_ref; }
         void   reference(double ref) { m_ref = ref; }
+        double saturateAt() const { return m_sat; }
         void   reset(void)
         {
             m_e[0] = 0.0, m_e[1] = 0.0, m_e[2] = 0.0;
@@ -32,12 +35,24 @@ namespace FW {
     {
         // Discrete PID controller difference equation
         m_e[0] = input - m_ref;
-        double y = m_y[0]
+        double y;
+        if (!m_saturated)
+            y = m_y[0]
                 + m_coef[0] * m_e[0]
                 + m_coef[1] * m_e[1]
                 + m_coef[2] * m_e[2];
-        if (std::abs(y) > m_sat)
+        else
+            y = m_csat[0] * m_e[0]
+                + m_csat[1] * m_e[1];
+
+        if (std::abs(y) <= m_sat)
+            m_saturated = false;
+        else
+        {
             y = copysign(m_sat, y);
+            m_saturated = true;
+        }
+
         m_e[2] = m_e[1];
         m_e[1] = m_e[0];
         m_y[0] = y;
